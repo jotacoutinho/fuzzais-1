97ccfa868ab59459a86459443ac8208aaf5a1192
AWZiegler <33697317+AWZiegler@users.noreply.github.com>
2017-11-15 15:49:21 -0200

    Class Test
    Example class

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 print("Bora porra")
+class Dog:
+
+    kind = 'canine'
+    tricks = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+
+
+
+def neuralMain():
+
+    d = Dog('xuxa')
+    e = Dog('joao')
+
+    d.add_trick('rolar')
+    e.add_trick('mamar')
+
+    print("\nClaramente universal")
+    print(d.kind)
+    print(e.kind)
+
+    print("\nClaramente único p/ cada instância")
+    print(d.name)
+    print(e.name)
+
+    print("\n Universal, mas não é obvio pois declaramos como se fosse por instância")
+    print(d.tricks)
+    print(e.tricks)
+
+    print
+
+if __name__ == "__main__":
+    neuralMain()
