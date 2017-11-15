dbd820602cd275c851b0a11bf3dcd3b9e7ffa873
João Pedro de Souza Coutinho <jpscoutinho@inf.ufrgs.br>
2017-11-15 19:56:56 -0200

    criação rede versão 1

--- neuronio.py
+++ neuronio.py
@@ -1,13 +1,31 @@
 import math
-
+import random
 
 class Neuron:
 
     def __init__(self):
         self.activation = 0
+        self.axonList = []
 
-    def setActivation(self, weightList, activationList):
+    def calculateActivation(self, weightList, activationList):
         inputSum = 0
         for i in range(0, len(activationList)):
             inputSum = inputSum + (weightList[i] * activationList[i])
-        self.activation = 1 / (1 + math.exp(-inputSum))
+        self.setActivation(1 / (1 + math.exp(-inputSum)))
+
+    # so sera usado (diretamente) pelo input
+    def setActivation(self, value):
+        self.activation = value
+
+    def setAxonList(self, n):
+        for i in range(0, n):
+            self.axonList[i] = random.random()
+
+    def setAxonWeight(self, i, value):
+        self.axonList[i] = value
+
+    def getAxon(self, i):
+        return self.axonList[i]
+
+    def getActivation(self):
+        return self.activation
--- redeneural.py
+++ redeneural.py
@@ -1,6 +1,39 @@
+import neuronio
+
 class NeuralNetwork:
 
-    def __init__(self, l, n):
+    def __init__(self, l, n, i):
         self.numLayers = l
         self.numNeurons = n
+        self.neuron = [[0 for i in range(0, n)] for i in range(0, l)]
+        self.input = [i]
 
+    # only internal layers
+    def createNetwork(self):
+        for l in range(0, self.numLayers):
+            for n in range(0, self.numNeurons):
+                self.neuron[n][l] = neuronio.Neuron()
+                if (l == 0):
+                    self.input[n].setAxonList(self.numNeurons)
+                else:
+                    self.neuron[n][l].setAxonList(self.numNeurons)
+
+    def createListsForPropagation(self, dest, l):
+        weightList = []
+        activationList = []
+        if(l == 0):
+            for n in range(0, self.numNeurons):
+                weightList.append(self.input[n].getAxon(dest))
+                activationList.append(self.input[n].getActivation())
+        else:
+            for n in range(0, self.numNeurons):
+                weightList.append(self.neuron[n][l-1].getAxon(dest))
+                activationList.append(self.neuron[n][l-1].getActivation())
+
+        return weightList, activationList
+
+    def propagateValues(self):
+        for l in range(0, self.numLayers):
+            for n in range(0, self.numNeurons):
+                weightList, activationList = self.createListsForPropagation(n, l)
+                self.neuron[n][l].setActivation(weightList, activationList)
